f793e06df6c9131330fcb64b271aafc1f7c0fea2
lubkepatryk <lubke.patryk@gmail.com>
2023-06-21 17:17:06 +0200

    App jsx changed to class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,85 +1,114 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import {ThreeDots} from 'react-loader-spinner';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
 import Modal from './Modal/Modal';
-import css from '../index.css'
+import css from '../index.css';
 
 const API_KEY = '35057683-7f01c4961286491953c834e45';
 
-export const App = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [modalImageURL, setModalImageURL] = useState('');
-
-  const handleSearchSubmit = query => {
-    setSearchQuery(query);
-    setPage(1);
-    setImages([]);
-  };
-
-  const handleLoadMore = () => {
-    setPage(prevPage => prevPage + 1);
-  };
-
-  const handleImageClick = imageURL => {
-    setModalImageURL(imageURL);
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-    setModalImageURL('');
-  };
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      if (searchQuery === '') return;
-
-      setIsLoading(true);
-
-      try {
-        const response = await fetch(
-          `https://pixabay.com/api/?q=${encodeURIComponent(
-            searchQuery
-          )}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-        );
-        const data = await response.json();
-
-        setImages(prevImages => [...prevImages, ...data.hits]);
-      } catch (error) {
-        console.log('Error fetching images:', error);
-      } finally {
-        setIsLoading(false);
-      }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: '',
+      images: [],
+      page: 1,
+      isLoading: false,
+      showModal: false,
+      modalImageURL: '',
     };
+  }
 
-    fetchImages();
-  }, [searchQuery, page]);
+  componentDidMount() {
+    this.fetchImages();
+  }
 
-  return (
-    <div className={css.App}>
-      <Searchbar onSubmit={handleSearchSubmit} />
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery || prevState.page !== this.state.page) {
+      this.fetchImages();
+    }
+  }
 
-      {isLoading ? (
-        <ThreeDots type="ThreeDots" color="#000" height={80} width={80} />
-      ) : (
-        <>
-          <ImageGallery images={images} onImageClick={handleImageClick}/>
+  handleSearchSubmit = query => {
+    this.setState({
+      searchQuery: query,
+      page: 1,
+      images: [],
+    });
+  };
 
-          {images.length > 0 && (
-            <Button onClick={handleLoadMore} />
-          )}
-        </>
-      )}
+  handleLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
 
-      {showModal && (
-        <Modal imageURL={modalImageURL} onClose={handleCloseModal} />
-      )}
-    </div>
-  );
-};
+  handleImageClick = imageURL => {
+    this.setState({
+      modalImageURL: imageURL,
+      showModal: true,
+    });
+  };
+
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false,
+      modalImageURL: '',
+    });
+  };
+
+  fetchImages = async () => {
+    const { searchQuery, page } = this.state;
+
+    if (searchQuery === '') return;
+
+    this.setState({
+      isLoading: true,
+    });
+
+    try {
+      const response = await fetch(
+        `https://pixabay.com/api/?q=${encodeURIComponent(
+          searchQuery
+        )}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      );
+      const data = await response.json();
+
+      this.setState(prevState => ({
+        images: [...prevState.images, ...data.hits],
+      }));
+    } catch (error) {
+      console.log('Error fetching images:', error);
+    } finally {
+      this.setState({
+        isLoading: false,
+      });
+    }
+  };
+
+  render() {
+    const { searchQuery, images, isLoading, showModal, modalImageURL } = this.state;
+
+    return (
+      <div className={css.App}>
+        <Searchbar onSubmit={this.handleSearchSubmit} />
+
+        {isLoading ? (
+          <ThreeDots type="ThreeDots" color="#000" height={80} width={80} />
+        ) : (
+          <>
+            <ImageGallery images={images} onImageClick={this.handleImageClick} />
+
+            {images.length > 0 && <Button onClick={this.handleLoadMore} />}
+          </>
+        )}
+
+        {showModal && <Modal imageURL={modalImageURL} onClose={this.handleCloseModal} />}
+      </div>
+    );
+  }
+}
+
+export default App;
